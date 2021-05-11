40aee2f8c718d923bc5c40c07c54a267c15341bb
Maxsuel Santos <santosmaxsuel17@gmail.com>
2021-05-11 12:27:30 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/max/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/max/catalog/resources/CategoryResource.java
@@ -1,9 +1,11 @@
 package com.max.catalog.resources;
 
+import com.max.catalog.dto.CategoryDTO;
 import com.max.catalog.entities.Category;
 import com.max.catalog.services.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +22,14 @@ public class CategoryResource {
     }
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> categories = categoryService.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> categories = categoryService.findAll();
         return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
+        CategoryDTO categoryDTO = categoryService.findById(id);
+        return ResponseEntity.ok(categoryDTO);
     }
 }
--- backend/src/main/java/com/max/catalog/services/CategoryService.java
+++ backend/src/main/java/com/max/catalog/services/CategoryService.java
@@ -1,11 +1,15 @@
 package com.max.catalog.services;
 
+import com.max.catalog.dto.CategoryDTO;
 import com.max.catalog.entities.Category;
+import com.max.catalog.exceptions.EntityNotFoundException;
 import com.max.catalog.repositories.CategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -17,7 +21,14 @@ public class CategoryService {
     }
 
     @Transactional(readOnly = true)
-    public List<Category> findAll() {
-        return categoryRepository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> categories = categoryRepository.findAll();
+        return categories.stream().map(CategoryDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id) {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Entity not found!"));
+        return new CategoryDTO(category);
     }
 }
